950549dc2caba8c9f736a87a1b7bece118c8a9ea
Oleksandr Dorobalo <al.dorobalo@gmail.com>
2025-02-27 18:56:13 +0100

    hero

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,134 +1,248 @@
-const init = async () => {
-  // Burger Menu Open //
+// Burger Menu Open //
+document.addEventListener("DOMContentLoaded", function () {
   // Выбираем бургер-кнопку и навигацию
   let burgerButton = document.getElementById("burgerButton");
-  let navigation = document.getElementById("primary-navigation");
-  let links = document.querySelectorAll(".navigation__link");
+  let mobileNav = document.querySelector(".mobile__nav");
+  let closeBtn = mobileNav.querySelector("#closeMenu");
+  let links = document.querySelectorAll(".nav__list a");
+  let body = document.querySelector("body");
+
 
   // Если бургер-кнопка существует, добавляем обработчик события
-  if (burgerButton && navigation) {
+  if (burgerButton) {
     burgerButton.addEventListener("click", function (e) {
       e.stopPropagation(); // Остановка всплытия события
-      burgerButton.classList.toggle("burger--active"); // Переключаем класс активности бургер-кнопки
-      navigation.classList.toggle("navigation--active"); // Переключаем класс активности навигации
+      // burgerButton.classList.add("burger--active");
+      mobileNav.classList.add("nav--active");
+      body.classList.add("lock");
     });
   }
 
-  // links.forEach((link) => {
-  //   link.addEventListener("click", function (e) {
-  //     burgerButton.classList.remove("burger--active");
-  //     navigation.classList.remove("navigation--active");
-  //     console.log("link", link);
-  //   });
-  // });
-  // Burger Menu Open //
+  if (closeBtn) {
+    closeBtn.addEventListener("click", function () {
+      // burgerButton.classList.remove("burger--active");
+      mobileNav.classList.remove("nav--active");
+      body.classList.remove("lock");
+    });
+  }
 
-  // Fixed Header //
-  const header = document.querySelector(".header");
-  const main = document.querySelector(".main");
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      // burgerButton.classList.remove("burger--active");
+      mobileNav.classList.remove("nav--active");
+      body.classList.remove("lock");
+    });
+  });
+});
+//
 
-  if (header && main) {
-    const headerH = header.offsetHeight; // Получаем высоту хедера
-    const checkScroll = (scrollOffset) => {
-      if (scrollOffset >= headerH) {
-        header.classList.add("fixed");
-        main.style.paddingTop = `${headerH}px`; // Устанавливаем верхний отступ
-      } else {
-        header.classList.remove("fixed");
-        main.style.paddingTop = `0`; // Убираем верхний отступ
-      }
-    };
+// Fixed header
+$(document).ready(function () {
+  var header = $(".header"),
+    main = $(".main"),
+    headerH = header.innerHeight(),
+    scrollOffset = $(window).scrollTop();
 
-    let scrollOffset = window.scrollY; // Начальное значение прокрутки
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
     checkScroll(scrollOffset);
+  });
 
-    window.addEventListener("scroll", () => {
-      scrollOffset = window.scrollY;
-      checkScroll(scrollOffset);
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= headerH) {
+      header.addClass("fixed");
+      main.css("padding-top", headerH); // Добавляем верхний отступ
+    } else {
+      header.removeClass("fixed");
+      main.css("padding-top", 0); // Убираем верхний отступ
+    }
+  }
+
+});
+//
+
+//faq collapse
+$(document).ready(function () {
+  // Обработчик клика на элемент с классом faq__title
+  $(".action").on("click", function () {
+    // Находим ближайший родительский элемент с классом faq__item
+    var $item = $(this).closest(".faq__item");
+    // Переключаем класс active у найденного элемента
+    $item.toggleClass("active");
+  });
+
+  // Обработчик клика на элемент с классом faq__btn
+  $(".faq__btn").on("click", function () {
+    // Находим ближайший родительский элемент с классом faq__item
+    var $item = $(this).closest(".faq__item");
+    // Переключаем класс active у найденного элемента
+    $item.toggleClass("active");
+  });
+});
+//faq collapse
+
+//
+function updateBackgrounds() {
+  const isMobile = window.innerWidth <= 768;
+
+  document.querySelectorAll(".product").forEach(section => {
+    const desktopBg = section.getAttribute("data-desktop-bg");
+    const mobileBg = section.getAttribute("data-mobile-bg");
+
+    if (isMobile && mobileBg) {
+      section.style.backgroundImage = `url(${mobileBg})`;
+    } else if (desktopBg) {
+      section.style.backgroundImage = `url(${desktopBg})`;
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", updateBackgrounds);
+window.addEventListener("resize", updateBackgrounds);
+//
+
+//swiper
+document.addEventListener("DOMContentLoaded", function () {
+  // Инициализация слайдера "customer"
+  if (document.querySelector("#news")) {
+    new Swiper("#news", {
+      observer: true,
+      observeParents: true,
+      loop: true,
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+      pagination: {
+        el: ".news-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".news-button-next",
+        prevEl: ".news-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1.2, // Один полный слайд и куски по бокам
+          spaceBetween: 20, // Расстояние между слайдами
+          centeredSlides: true,
+        },
+        560: {
+          slidesPerView: 1.5, // Один полный слайд и куски по бокам
+          centeredSlides: true,
+          spaceBetween: 20, // Расстояние между слайдами
+        },
+        768: {
+          slidesPerView: 2, // Один полный слайд и куски по бокам
+          centeredSlides: false,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
     });
   }
-  // Fixed Header //
 
-  // select
-  var x, i, j, l, ll, selElmnt, a, b, c;
-  /*look for any elements with the class "custom-select":*/
-  x = document.getElementsByClassName("custom-select");
-  l = x.length;
-  for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-    /*for each element, create a new DIV that will act as the selected item:*/
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /*for each element, create a new DIV that will contain the option list:*/
-    b = document.createElement("UL");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < ll; j++) {
-      /*for each option in the original select element,
-      create a new DIV that will act as an option item:*/
-      c = document.createElement("LI");
-      c.innerHTML = selElmnt.options[j].innerHTML;
-      c.addEventListener("click", function (e) {
-        /*when an item is clicked, update the original select box,
-        and the selected item:*/
-        var y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < sl; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
+  if (document.querySelector("#reviews")) {
+    new Swiper("#reviews", {
+      observer: true,
+      observeParents: true,
+      loop: true,
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+      pagination: {
+        el: ".reviews-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".reviews-button-next",
+        prevEl: ".reviews-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1.2, // Один полный слайд и куски по бокам
+          spaceBetween: 20, // Расстояние между слайдами
+          centeredSlides: true,
+        },
+        560: {
+          slidesPerView: 1.5, // Один полный слайд и куски по бокам
+          centeredSlides: true,
+          spaceBetween: 20, // Расстояние между слайдами
+        },
+        768: {
+          slidesPerView: 2, // Один полный слайд и куски по бокам
+          centeredSlides: false,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20, // Расстояние между слайдами
+        },
+      },
+    });
+  }
+
+});
+// swiper
+
+//
+function addField() {
+  const container = document.getElementById("kadastrinis-fields");
+  const fieldCount = container.getElementsByClassName("form-group").length;
+
+  if (fieldCount < 3) {
+      const newField = document.createElement("div");
+      newField.classList.add("form-group");
+      newField.innerHTML = `
+          <input type="text" name="kadastrinis[]" placeholder="XXXX/ZZZZ:YYYY">
+          <button type="button" class="icon remove-field" onclick="removeField(this)">-</button>
+      `;
+      container.appendChild(newField);
+  }
+
+  updateButtons();
+}
+
+function removeField(button) {
+  button.parentElement.remove();
+  updateButtons();
+}
+
+function updateButtons() {
+  const container = document.getElementById("kadastrinis-fields");
+  const fields = container.getElementsByClassName("form-group");
+  const addButton = document.querySelector(".add-field");
+
+  if (fields.length >= 3 && addButton) {
+      addButton.style.display = "none";
+  } else if (fields.length < 3 && !addButton) {
+      const lastField = fields[fields.length - 1];
+      const newAddButton = document.createElement("button");
+      newAddButton.type = "button";
+      newAddButton.classList.add("add-field");
+      newAddButton.innerText = "+";
+      newAddButton.onclick = addField;
+      lastField.appendChild(newAddButton);
+  } else if (fields.length < 3 && addButton) {
+      addButton.style.display = "flex";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  function updateRequiredLabels() {
+      document.querySelectorAll(".form-group label").forEach(function (label) {
+          let input = label.nextElementSibling;
+          if (input && input.required) {
+              label.classList.add("required");
+          } else {
+              label.classList.remove("required");
           }
-        }
-        h.click();
       });
-      b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
-      /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
-    });
   }
-  function closeAllSelect(elmnt) {
-    /*a function that will close all select boxes in the document,
-    except the current select box:*/
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-      if (elmnt == y[i]) {
-        arrNo.push(i)
-      } else {
-        y[i].classList.remove("select-arrow-active");
-      }
-    }
-    for (i = 0; i < xl; i++) {
-      if (arrNo.indexOf(i)) {
-        x[i].classList.add("select-hide");
-      }
-    }
-  }
-  /*if the user clicks anywhere outside the select box,
-  then close all select boxes:*/
-  document.addEventListener("click", closeAllSelect);
-  // select
-};
 
-window.onload = init;
+  updateRequiredLabels();
+});
+//
